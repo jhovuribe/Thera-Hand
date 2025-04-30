3ea7d6559bf8f33535723c59051d193ff158a258
cadenroberts <130518994+cadenroberts@users.noreply.github.com>
2025-04-29 22:57:05 -0700

    Update data.sql

--- Development/Thera_app/sql/data.sql
+++ Development/Thera_app/sql/data.sql
@@ -1,9 +1,11 @@
 -- Clean up existing data
+DELETE FROM messages;
 DELETE FROM doctor_patients;
 DELETE FROM fingers;
 DELETE FROM doctors;
 DELETE FROM patients;
 DELETE FROM users;
+
 
 -- Insert users (patients)
 INSERT INTO users (role, data) VALUES
@@ -18,8 +20,8 @@ SELECT id FROM users WHERE role = 'patient';
 
 -- Insert users (doctors)
 INSERT INTO users (role, data) VALUES
-  ('doctor', '{"email": "dr.tan@therahand.com","name": "Dr. Tan"}'),
-  ('doctor', '{"email": "dr.lu@therahand.com","name": "Dr. Lu"}');
+  ('doctor', '{"email": "dr.harrison@therahand.com","password": "$2b$10$3xm.L4KzlLj25ISSPKtYG.LtTucGZKyFQ1WyakPzqJ5FEdVPf80QC","name": "Dr. Harrison"}'),
+  ('doctor', '{"email": "dr.lu@therahand.com","password": "$2b$10$WHaord9QWJQD2wr2BdY6ROSw21w6LF5tPiwoA7u67PtQKE.XuEnOi","name": "Dr. Lu"}');
 
 -- Promote those users to doctors
 INSERT INTO doctors (id)
@@ -36,14 +38,41 @@ INSERT INTO fingers (patient_id, data) VALUES
 -- Assign doctor-patient relationships
 INSERT INTO doctor_patients (doctor_id, patient_id) VALUES
   (
-    (SELECT id FROM users WHERE data->>'email' = 'dr.tan@therahand.com'),
+    (SELECT id FROM users WHERE data->>'email' = 'dr.harrison@therahand.com'),
     (SELECT id FROM users WHERE data->>'email' = 'aliyaa@therahand.com')
   ),
   (
-    (SELECT id FROM users WHERE data->>'email' = 'dr.tan@therahand.com'),
+    (SELECT id FROM users WHERE data->>'email' = 'dr.harrison@therahand.com'),
     (SELECT id FROM users WHERE data->>'email' = 'ethan@therahand.com')
   ),
   (
     (SELECT id FROM users WHERE data->>'email' = 'dr.lu@therahand.com'),
     (SELECT id FROM users WHERE data->>'email' = 'jhovanny@therahand.com')
   );
+
+-- Insert fluff messages
+INSERT INTO messages (sender_id, recipient_id, content) VALUES
+  -- dr. harrison to Aliyaa
+  ((SELECT id FROM users WHERE data->>'email' = 'dr.harrison@therahand.com'),
+   (SELECT id FROM users WHERE data->>'email' = 'aliyaa@therahand.com'),
+   'Hello Aliyaa, how is your therapy going this week?'),
+
+  -- Aliyaa to dr. harrison
+  ((SELECT id FROM users WHERE data->>'email' = 'aliyaa@therahand.com'),
+   (SELECT id FROM users WHERE data->>'email' = 'dr.harrison@therahand.com'),
+   'Hi Dr. Harrison! I''m feeling better, thanks for checking in.'),
+
+  -- Dr. Lu to Jhovanny
+  ((SELECT id FROM users WHERE data->>'email' = 'dr.lu@therahand.com'),
+   (SELECT id FROM users WHERE data->>'email' = 'jhovanny@therahand.com'),
+   'Jhovanny, please upload your finger movement logs.'),
+
+  -- Jhovanny to Dr. Lu
+  ((SELECT id FROM users WHERE data->>'email' = 'jhovanny@therahand.com'),
+   (SELECT id FROM users WHERE data->>'email' = 'dr.lu@therahand.com'),
+   'Sure, I''ll do that by tonight.'),
+
+  -- Ethan to dr. harrison
+  ((SELECT id FROM users WHERE data->>'email' = 'ethan@therahand.com'),
+   (SELECT id FROM users WHERE data->>'email' = 'dr.harrison@therahand.com'),
+   'Hi Doctor, my index finger still hurts a little. Any tips?');
