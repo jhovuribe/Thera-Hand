4457f51655d08d2b70b248042c632512f5ef7b10
cadenroberts <130518994+cadenroberts@users.noreply.github.com>
2025-04-29 22:56:08 -0700

    Update App.jsx
    extend route functionality

--- Development/Thera_app/src/App.jsx
+++ Development/Thera_app/src/App.jsx
@@ -1,4 +1,5 @@
 import SignIn from './SignIn.jsx';
+import CreateUser from './CreateUser.jsx'
 import Home from './Home.jsx';
 import Unauthorized from './Unauthorized.jsx';
 import ProtectedRoute from './ProtectedRoute.jsx';
@@ -20,6 +21,11 @@ function App({ router: Router = BrowserRouter }) {
           <Route path="/home" element={
             <ProtectedRoute>
               <Home />
+            </ProtectedRoute>
+          }/>
+          <Route path="/create" element={
+            <ProtectedRoute requiredRole="doctor">
+              <CreateUser/>
             </ProtectedRoute>
           }/>
           <Route path="/unauthorized" element={<Unauthorized />} />
